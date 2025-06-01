fe5054049cfe803fc32d2358d4be212fe5e4315f
AkoSiJacobDeveloper <pauljacobtocmo96@gmail.com>
2025-06-01 21:59:49 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -44,8 +44,9 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            // 'url' => env('DB_URL'),
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
